9efbc17de17821d4d7ae903bf3f29633fbae0f35
ruel <ruelpaquera@gmail.com>
2019-06-04 13:10:28 +0800

    update code for created function in bioscan in c++

--- src/scan.cpp
+++ src/scan.cpp
@@ -1,21 +1,43 @@
-#include <iomanip>
-#include <sstream>
-#include <zlib.h>
-#include <vector>
-
+#include "fingerprint.h"
+#include "selection.h"  
 #include "helpers.h"
+#include "scan.h"
 
-#include <dpfpdd.h>
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
-#include <locale.h>
-
-#include <v8.h>
 
 using namespace v8;
 using v8::FunctionTemplate;
 
+typedef struct __FINGER_DATA__ {
+    Nan::Persistent<Function> callback;
+    unsigned char *fingerprint_data;
+    int fingerprint_size;
+    int result;
+    unsigned char *fingerprint_img;
+} FINGER_DATA;
+
+NAN_METHOD(startScan) {
+    int returns;
+    int bDone = 0;
+    FINGER_DATA *pdata;
+    pdata = new FINGER_DATA;
+    
+    if(!pdata)
+        goto error;
+        
+    pdata->fingerprint_data = NULL;
+    pdata->fingerprint_size = 0;
+    pdata->result = 0;
+
+    pdata->callback.Reset(v8::Local<v8::Function>::Cast(info[1]));
+    
+    while(!bDone){
+
+        
+    }
+
+
+    returns = true;
+error:
+    info.GetReturnValue().Set(Nan::New(returns));
+    return;
+}
--- src/scan.h
+++ src/scan.h
@@ -1,35 +1,3 @@
-#include "fingerprint.h"
-#include "selection.h"  
-#include "helpers.h"
+#include <nan.h>
 
-#include <dpfpdd.h>
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-
-using namespace v8;
-using v8::FunctionTemplate;
-
-typedef struct __FINGER_DATA__ {
-    Nan::Persistent<Function> callback;
-    unsigned char *fingerprint_data;
-    size_t fingerprint_size;
-    int result;
-    unsigned char *fingerprint_img;
-} FINGER_DATA;
-
-NAN_METHOD(checkharware) {
-    int returns;
-    FINGER_DATA *pdata;
-
-    pdata->fingerprint_data = NULL;
-    pdata->fingerprint_size = NULL;
-    pdata->result = 0;
-
-    returns = true;
-error:
-    info.GetReturnValue().Set(Nan::New(returns));
-    return;
-}
+Nan::NAN_METHOD_RETURN_TYPE startScan(Nan::NAN_METHOD_ARGS_TYPE info);
