fc3ad85ef39eb3504b13341649378d228c87c94d
ruelpaquera <ruelpaquera@gmail.com>
2021-06-26 18:16:32 +0800

    update

---
+++ build/Release/.deps/Release/obj.target/biometric/src/new/verify.o.d
@@ -0,0 +1,75 @@
+cmd_Release/obj.target/biometric/src/new/verify.o := g++ '-DNODE_GYP_MODULE_NAME=biometric' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' '-DOLD_UV_RUN_SIGNATURE' -I/home/ruel/.cache/node-gyp/8.17.0/include/node -I/home/ruel/.cache/node-gyp/8.17.0/src -I/home/ruel/.cache/node-gyp/8.17.0/deps/openssl/config -I/home/ruel/.cache/node-gyp/8.17.0/deps/openssl/openssl/include -I/home/ruel/.cache/node-gyp/8.17.0/deps/uv/include -I/home/ruel/.cache/node-gyp/8.17.0/deps/zlib -I/home/ruel/.cache/node-gyp/8.17.0/deps/v8/include -I../node_modules/nan -I/opt/Crossmatch/urusdk-linux/Include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/biometric/src/new/verify.o.d.raw   -c -o Release/obj.target/biometric/src/new/verify.o ../src/new/verify.cpp
+Release/obj.target/biometric/src/new/verify.o: ../src/new/verify.cpp \
+ ../src/new/verify.h ../node_modules/nan/nan.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/node_version.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/uv.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/uv/errno.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/uv/version.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/uv/unix.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/uv/threadpool.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/uv/linux.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/node.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/v8.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/v8-version.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/v8config.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/v8-platform.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/node_version.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/node_buffer.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/node.h \
+ /home/ruel/.cache/node-gyp/8.17.0/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_private.h \
+ ../node_modules/nan/nan_typedarray_contents.h \
+ ../node_modules/nan/nan_json.h \
+ /opt/Crossmatch/urusdk-linux/Include/dpfpdd.h ../src/new/helpers.h \
+ /opt/Crossmatch/urusdk-linux/Include/dpfj.h \
+ /opt/Crossmatch/urusdk-linux/Include/dpfj_compression.h \
+ /opt/Crossmatch/urusdk-linux/Include/dpfj_quality.h ../src/new/capture.h \
+ ../src/new/base64.h
+../src/new/verify.cpp:
+../src/new/verify.h:
+../node_modules/nan/nan.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/node_version.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/uv.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/uv/errno.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/uv/version.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/uv/unix.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/uv/threadpool.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/uv/linux.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/node.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/v8.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/v8-version.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/v8config.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/v8-platform.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/node_version.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/node_buffer.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/node.h:
+/home/ruel/.cache/node-gyp/8.17.0/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
+/opt/Crossmatch/urusdk-linux/Include/dpfpdd.h:
+../src/new/helpers.h:
+/opt/Crossmatch/urusdk-linux/Include/dpfj.h:
+/opt/Crossmatch/urusdk-linux/Include/dpfj_compression.h:
+/opt/Crossmatch/urusdk-linux/Include/dpfj_quality.h:
+../src/new/capture.h:
+../src/new/base64.h:
